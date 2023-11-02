d841113ce687405c3cbc57ad6fdf83115b7ebb87
tpkowastaken <protitom@gmail.com>
2023-11-03 00:04:46 +0100

    gyro works, bluetooth works

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,69 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:trebesin_rc_auto/SelectBondedDevicePage.dart';
 
-void bluetooth() async {
-  if (!await Permission.bluetooth.request().isGranted) {
-    await Permission.bluetooth.request();
-  }
-  if (!await Permission.bluetoothAdvertise.request().isGranted) {
-    await Permission.bluetoothAdvertise.request();
-  }
-  if (!await Permission.bluetoothConnect.request().isGranted) {
-    await Permission.bluetoothConnect.request();
-  }
-  if (!await Permission.bluetoothScan.request().isGranted) {
-    await Permission.bluetoothScan.request();
-  }
-  if (!await Permission.bluetooth.request().isGranted) {
-    return;
-  }
-  if (!await Permission.bluetoothAdvertise.request().isGranted) {
-    return;
-  }
-  if (!await Permission.bluetoothConnect.request().isGranted) {
-    return;
-  }
-  if (!await Permission.bluetoothScan.request().isGranted) {
-    return;
-  }
-
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-  flutterBlue.startScan(timeout: const Duration(seconds: 4));
-  flutterBlue.startScan(timeout: const Duration(seconds: 4));
-
-  //Listen to scan results
-  var subscription = flutterBlue.scanResults.listen((results) {
-    // do something with scan results
-    for (ScanResult r in results) {
-      if (r.device.id.id == "00:22:12:01:13:6A") {
-        print("found");
-        print("found");
-        print("found");
-        print("found");
-        print("found");
-      }
-      if (r.device.name.trim() != "") {
-        print('${r.device.name} found! rssi: ${r.rssi}');
-      }
-    }
-  });
-  /*
-    // Connect to the device
-  await device.connect();
-
-  // Disconnect from device
-  device.disconnect();*/
+BluetoothConnection? connection;
+void send(String text) async {
+  connection?.output.add(Uint8List.fromList(utf8.encode("$text\r\n")));
+  await connection?.output.allSent;
 }
 
-void main() {
+void bluetooth(BuildContext context) async {
+  if (connection != null) {
+    connection?.dispose();
+    connection = null;
+  }
+  await Permission.bluetooth.request();
+  await Permission.bluetoothAdvertise.request();
+  await Permission.bluetoothConnect.request();
+  await Permission.bluetoothScan.request();
+  final BluetoothDevice? selectedDevice = await Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) {
+        return const SelectBondedDevicePage(checkAvailability: false);
+      },
+    ),
+  );
+  BluetoothDevice? server = selectedDevice;
+  if (server == null) {
+    print('Connect -> no device selected');
+    return;
+  }
+
+  if (selectedDevice != null) {
+    if (kDebugMode) {
+      print('Connect -> selected ${selectedDevice.address}');
+    }
+    BluetoothConnection.toAddress(server.address).then((localConnection) {
+      if (kDebugMode) {
+        print('Connected to the device');
+      }
+      connection = localConnection;
+
+      connection!.input!.listen(null).onDone(() {
+        // Example: Detect which side closed the connection
+        // There should be `isDisconnecting` flag to show are we are (locally)
+        // in middle of disconnecting process, should be set before calling
+        // `dispose`, `finish` or `close`, which all causes to disconnect.
+        // If we except the disconnection, `onDone` should be fired as result.
+        // If we didn't except this (no flag set), it means closing by remote.
+        print('Disconnected!');
+      });
+    }).catchError((error) {
+      print('Cannot connect, exception occured');
+      print(error);
+    });
+  } else {
+    print('Connect -> no device selected');
+  }
+}
+
+void main() async {
   runApp(const MyApp());
 }
 
@@ -79,88 +86,122 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double x = 0, y = 0, z = 0;
+  @override
+  void initState() {
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      x += event.x;
+      y += event.y;
+      z += event.z;
+
+      //rough calculation, you can use
+      //advance formula to calculate the orentation
+      if (z > 0) {
+        direction = "left";
+      } else if (z < 0) {
+        direction = "right";
+      }
+
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and disconnect
+    if (connection != null) {
+      connection?.dispose();
+      connection = null;
+    }
+    super.dispose();
+  }
+
+  String direction = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: const [
+        actions: [
           IconButton(
-            icon: Icon(Icons.bluetooth),
-            onPressed: bluetooth,
+            icon: const Icon(Icons.bluetooth),
+            onPressed: () => bluetooth(context),
           ),
         ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("RC Auto Ovladač"),
       ),
-      body: Center(
-        child: SizedBox(
-          height: 300,
-          width: 300,
-          child: Stack(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
             children: [
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    //left
-                    MaterialButton(
-                      shape: const CircleBorder(side: BorderSide(color: Colors.transparent)),
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {},
-                      child: const Icon(
-                        Icons.arrow_circle_left_outlined,
-                        size: 90,
-                      ),
-                    ),
-
-                    //right
-                    MaterialButton(
-                      shape: const CircleBorder(side: BorderSide(color: Colors.transparent)),
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {},
-                      child: const Icon(
-                        Icons.arrow_circle_right_outlined,
-                        size: 90,
-                      ),
-                    ),
-                  ],
-                ),
+              Text(
+                "Ovládání",
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    //up
-                    MaterialButton(
-                      shape: const CircleBorder(side: BorderSide(color: Colors.transparent)),
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {},
-                      child: const Icon(
-                        Icons.arrow_circle_up,
-                        size: 90,
-                      ),
+              Text(
+                "Připojeno: ${connection?.isConnected ?? false}",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Text(
+                "Gyroskop: $direction",
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    x = 0;
+                    y = 0;
+                    z = 0;
+                  },
+                  child: Text("Reset")),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //up
+                  MaterialButton(
+                    shape: const CircleBorder(side: BorderSide(color: Colors.transparent)),
+                    padding: const EdgeInsets.all(0),
+                    onPressed: () {
+                      send("W");
+                    },
+                    child: const Icon(
+                      Icons.arrow_circle_up,
+                      size: 90,
                     ),
+                  ),
 
-                    //down
+                  //down
 
-                    MaterialButton(
-                      shape: const CircleBorder(side: BorderSide(color: Colors.transparent)),
-                      padding: const EdgeInsets.all(0),
-                      onPressed: () {},
-                      child: const Icon(
-                        Icons.arrow_circle_down,
-                        size: 90,
-                      ),
+                  MaterialButton(
+                    shape: const CircleBorder(side: BorderSide(color: Colors.transparent)),
+                    padding: const EdgeInsets.all(0),
+                    onPressed: () {
+                      send("B");
+                    },
+                    child: const Icon(
+                      Icons.arrow_circle_down,
+                      size: 90,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
